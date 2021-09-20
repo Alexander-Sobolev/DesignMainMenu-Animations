aacca0fab29d67801b9f36f1e7d8f102b035dbe0
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-20 18:50:11 +0300

    Initial Commit

---
+++ DesignMainMenu+Animations/DesignMainMenu_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignMainMenu_AnimationsApp.swift
+//  DesignMainMenu+Animations
+//
+//  Created by Alexander Sobolev on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignMainMenu_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
